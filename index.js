7e3b4a7954e0ba4a55e1ff74f16718e389c1981e
Rohit <85179514+rohit1919119@users.noreply.github.com>
2022-06-13 22:47:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+/* Note Codepen is importing the jQuery Background Video plugin - https://github.com/BGStock/jquery-background-video */
+$(document).ready(function() {
+    //Set the carousel options
+    $('#quote-carousel').carousel({
+      pause: true,
+      interval: 4000,
+    });
+  });
+  
+
+  
+$(document).ready(function () {
+    //DropDown input - select
+    $(".t-dropdown-input").on("click", function () {
+      $(this).parent().next().slideDown("fast");
+    });
+  
+    $(".t-select-btn").on("click", function () {
+      $(".t-dropdown-list").slideUp("fast");
+  
+      if (!$(this).prev().attr("disabled")) {
+        $(this).prev().trigger("click");
+      }
+    });
+  
+    $(".t-dropdown-input").width(
+      $(".t-dropdown-select").width() - $(".t-select-btn").width() - 13
+    );
+  
+    $(".t-dropdown-list").width($(".t-dropdown-select").width());
+  
+    $(".t-dropdown-input").val("");
+  
+    $("li.t-dropdown-item").on("click", function () {
+      var text = $(this).html();
+      $(this).parent().prev().find(".t-dropdown-input").val(text);
+      $(".t-dropdown-list").slideUp("fast");
+    });
+  
+    $(document).on("click", function (event) {
+      if ($(event.target).closest(".t-dropdown-input, .t-select-btn").length)
+        return;
+      $(".t-dropdown-list").slideUp("fast");
+      event.stopPropagation();
+    });
+    // END //
+  });
